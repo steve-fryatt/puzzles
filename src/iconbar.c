676ed93c95124b682998cb21069d81a05420a7b2
Steve Fryatt <info@stevefryatt.org.uk>
2025-10-19 00:52:25 +0100

    Move website link to HTTPS

--- src/iconbar.c
+++ src/iconbar.c
@@ -188,7 +188,7 @@ static osbool iconbar_proginfo_web_click(wimp_pointer *pointer)
 {
 	char	temp_buf[256];
 
-	msgs_lookup("SupportURL:http://www.stevefryatt.org.uk/risc-os/games", temp_buf, sizeof(temp_buf));
+	msgs_lookup("SupportURL:https://www.stevefryatt.org.uk/risc-os/games", temp_buf, sizeof(temp_buf));
 	url_launch(temp_buf);
 
 	if (pointer->buttons == wimp_CLICK_SELECT)
